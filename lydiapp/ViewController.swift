a7b21ef8ab94916cff3f25847bcdc66c5f615184
Guik <gbisiaux59@icloud.com>
2025-07-30 11:12:08 +0200

    Initial Commit

---
+++ lydiapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lydiapp
+//
+//  Created by Guillaume Bisiaux on 30/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
